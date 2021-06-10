7fe54c9e6aca67a7053e94b8dcd3d6318266e94a
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-06-10 20:32:02 +0200

    First commit

---
+++ Edunova03/src/edunova/K01_Varijable.java
@@ -0,0 +1,43 @@
+package edunova;
+
+public class K01_Varijable {
+	
+	public static void main(String[] args) {
+		// deklarirali varijablu tipa cijeli broj (int) i dodjelili vrijednost 1
+		int i=1;
+		
+		// deklaracija varijable
+		int t;
+		
+		// dodjeljivanje vrijednosti
+		t=7;
+		
+		//korištenje varijable
+		System.out.println(t);
+		
+		// èitati primitivni tipovi podataka
+		//https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+		
+		/*
+		 * 
+		 * više redova
+		 * 
+		 */
+		
+		/*
+		 
+		 više redova
+		 
+		 */
+		
+//		Hello 
+//		Edunova
+		
+		int k,l,m;
+		
+		int v=2,p=0;
+		
+		
+	}
+
+}
