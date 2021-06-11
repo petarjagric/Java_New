a2af1264e6fa628b13061527cbf417a3bc7cc908
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-06-11 23:19:46 +0200

    First commit
    First task

---
+++ Zadaca/src/module-info.java
@@ -0,0 +1,3 @@
+module zadaca {
+	requires java.desktop;
+}
---
+++ Zadaca/src/zadaca/Z_01.java
@@ -0,0 +1,50 @@
+package zadaca;
+
+import javax.swing.JOptionPane;
+
+public class Z_01 {
+	public static void main(String[] args) {
+		
+		/* Zadatak 1
+		 korisnik unosi cijeli broj
+		 za parni broj se ispisuje Osijek
+		 za neparni broj se ispisuje Donji Miholjac
+		
+		int a;
+		
+		a = Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli braoj"));
+		
+		if (a%2==0) {
+			System.out.println("Osijek");
+		}else {
+			System.out.println("Donji Miholjac");
+		}*/
+		
+		
+		
+		
+		
+		/*
+		 dodatno zadatak 1
+	 	 U sluèaju unosa parnog broja tražiti unos
+		 dva cijela broja i ispisati zbroj
+		 u sluèaju unosa neparnog broja tražiti unos
+	 	 decimalnog broja i ispisati samo cijeli dio
+		*/
+		
+		int a;
+		
+		a = Integer.parseInt(JOptionPane.showInputDialog("Unesi cijeli broj"));
+		
+		if (a%2==0) {
+			int b;
+			b = Integer.parseInt(JOptionPane.showInputDialog("Unesi drugi cijeli broj"));	
+			System.out.println(a+b);
+		}else {
+			float c;
+			c = Float.parseFloat(JOptionPane.showInputDialog("Unos decimalong broja"));
+			System.out.println((a+c)%2==0);
+		}
+	}
+
+}
