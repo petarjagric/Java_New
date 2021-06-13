a027bed7842a40f0c9102b24dcf0a5968621f66e
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-06-14 00:02:00 +0200

    Task completed

--- Zadaca/src/zadaca/Z_04.java
+++ Zadaca/src/zadaca/Z_04.java
@@ -4,25 +4,28 @@ import javax.swing.JOptionPane;
 
 public class Z_04 {
 	public static void main(String[] args) {
-		
+
 		int a;
 		a = Integer.parseInt(JOptionPane.showInputDialog("unestie broj od 1-4"));
-		
-		if (a<1 || a>4) {
+
+		float b, c;
+		b = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
+		c = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
+
+		if (a < 1 || a > 4) {
 			System.out.println("Greška");
-		} else {
-			
-			float b,c;
-			b = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
-			c = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
-			
-			float zbroj = b + c;
-			float oduzimanje = b - c;
-			float umnozak = b * c;
-			float dijeljne = b / c;
-			
-			
 		}
+
+		if (a == 1) {
+			System.out.println(b + c);
+		} else if (a == 2) {
+			System.out.println(b - c);
+		} else if (a == 3) {
+			System.out.println(b * c);
+		} else if (a == 4) {
+			System.out.println(b / c);
+		}
+
 	}
 
 }
