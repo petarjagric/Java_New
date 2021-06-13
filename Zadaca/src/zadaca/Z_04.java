159e687846a92333f6b3a2e27e7f96f201c8f668
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-06-14 00:17:41 +0200

    task completed

--- Zadaca/src/zadaca/Z_04.java
+++ Zadaca/src/zadaca/Z_04.java
@@ -4,17 +4,18 @@ import javax.swing.JOptionPane;
 
 public class Z_04 {
 	public static void main(String[] args) {
-
+		
 		int a;
-		a = Integer.parseInt(JOptionPane.showInputDialog("unestie broj od 1-4"));
-
-		float b, c;
-		b = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
-		c = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
-
+		a = Integer.parseInt(JOptionPane.showInputDialog("unestie broj od 1-4 \n1 - zbrajanje \n2 - oduzimanje\n3 - mnozenje\n4 - dijeljenje"));
+		
+		
 		if (a < 1 || a > 4) {
 			System.out.println("Greška");
 		}
+		
+		float b,c;
+		b = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
+		c = Float.parseFloat(JOptionPane.showInputDialog("Unesite decimalni broj"));
 
 		if (a == 1) {
 			System.out.println(b + c);
