16d37eb9b272664b22c26d98e86cd74d866a6c7f
Petar Jagrić <83279909+petarjagric@users.noreply.github.com>
2021-06-20 22:54:55 +0200

    Task completed

--- Edunova06/src/edunova/Zadatak6.java
+++ Edunova06/src/edunova/Zadatak6.java
@@ -5,10 +5,22 @@ import java.util.Arrays;
 import javax.swing.JOptionPane;
 
 public class Zadatak6 {
-
-	// Program unosi 10 cijelih brojeva od korisnika
-	// u jednodimenzionalni niz
-	// Program ispisuje sve elemente niza
-
+		public static void main(String[] args) {
+			// Program unosi 10 cijelih brojeva od korisnika
+			// u jednodimenzionalni niz
+			// Program ispisuje sve elemente niza
+			
+			int niz [] = new int [10];
+					
+			for (int i = 0; i < niz.length; i++) {
+				niz[i] = Integer.parseInt(JOptionPane.showInputDialog("Unesi broj"));
+			}
+			
+			for (int i = 0; i < niz.length; i++) {
+				System.out.print(niz[i] + " ");
+			
+		}
 	
+	
+}
 }
